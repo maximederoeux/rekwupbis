eb8fba8227769be5339260cd46b8ee9f0b2666d6
maximederoeux <mderoeux@yahoo.com>
2016-01-11 11:43:09 +0100

    More design on events and users

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -71,7 +71,7 @@ class EventsController < ApplicationController
   def update
     respond_to do |format|
       if @event.update(event_params)
-        format.html { redirect_to :back, notice: t("notice.event_updated") }
+        format.html { redirect_to @event, notice: t("notice.event_updated") }
         format.json { render :show, status: :ok, location: @event }
       else
         format.html { render :edit }
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,9 @@ class Event < ActiveRecord::Base
 	belongs_to :organizer, :class_name => "User"
 	has_many :orders
 
+	validates :event_name, presence: true
+	validates :event_start_date, presence: true
+	validates :event_end_date, presence: true
+	validates :expected_pax, presence: true
+
 end
