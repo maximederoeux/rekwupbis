a768ab8a354e2764d434a5b7aed2bc5eb4e8fcb3
maximederoeux <mderoeux@yahoo.com>
2016-01-11 12:48:50 +0100

    Users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
     @company = @users.where(:company => true)
     @non_profit = @users.where(:non_profit => true)
     @institution = @users.where(:institution => true)
+    @client_no_status = @client.where.not(:institution => true).where.not(:company => true).where.not(:individual => true).where.not(:non_profit => true).where.not(:staff => true)
+    @no_status_at_all = @client_no_status.where.not(:admin => true)
   end
 
   def show
@@ -38,7 +40,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.update(user_params)
         if current_user.admin
-        format.html { redirect_to edit_user_path(@user), notice: t("notice.profile_updated")}
+        format.html { redirect_to @user, notice: t("notice.profile_updated")}
         else
         format.html { redirect_to @user, notice: t("notice.profile_updated")}
         end
