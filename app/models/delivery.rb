7d99f5fe15239c57cceb8d24bc603966ccdef469
maximederoeux <mderoeux@yahoo.com>
2016-01-12 22:51:28 +0100

    Started with delivery design

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,6 +1,10 @@
 class Delivery < ActiveRecord::Base
 	belongs_to :offer
 
+
+	scope :this_day, lambda {where(:delivery_date => Date.today)}
+	scope :tomorrow, lambda {where(:delivery_date => Date + 1)}
+
 	def offer_boxes
 		self.offer.offer_boxes
 	end
