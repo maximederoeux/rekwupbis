e5a70f2d6e073db53f878f07def1465d95e37387
maximederoeux <mderoeux@yahoo.com>
2016-01-14 10:35:09 +0100

    Added datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,25 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-datepicker
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require bootstrap-datepicker
 //= require_tree .
-
-
-// $.fn.datepicker.dates['en'] = {
-//     days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-//     daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-//     daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
-//     months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-//     monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-//     today: "Today",
-//     clear: "Clear",
-//     format: "mm/dd/yyyy",
-//     titleFormat: "MM yyyy", /* Leverages same syntax as 'format' */
-//     weekStart: 0
-// };
-
 
 
 $(document).ready(function(){
