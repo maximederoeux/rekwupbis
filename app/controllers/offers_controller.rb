343a1383dedf5a90d6d3d91d5fb4913bd4be5e46
maximederoeux <mderoeux@yahoo.com>
2016-01-22 00:27:24 +0100

    Mailer config

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -103,10 +103,10 @@ class OffersController < ApplicationController
            
     respond_to do |format|
       if @offer.update(offer_params)
-        # if @offer.send_email
-        #   UserMailer.offer_email(@user, @offer).deliver_later
-        #   @offer.update_attributes(:send_email => false)
-        # end
+        if @offer.send_email
+          UserMailer.offer_email(@user, @offer).deliver_later
+          @offer.update_attributes(:send_email => false)
+        end
         format.html { redirect_to @offer, notice: 'Offer was successfully updated.' }
         format.json { render :show, status: :ok, location: @offer }
       else
