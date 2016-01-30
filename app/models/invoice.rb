1a1350f3c18196c32f59e7d19006aea1f68419a3
maximederoeux <mderoeux@yahoo.com>
2016-01-30 22:47:16 +0100

    Invoices totals

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,9 @@
 class Invoice < ActiveRecord::Base
-		belongs_to :client, :class_name => "User"
-		belongs_to :offer
+	belongs_to :client, :class_name => "User"
+	belongs_to :offer
+
+	has_many :sorting_details, through: :offer
+	has_many :offer_articles, through: :offer
 
 	scope :debit, lambda {where(:doc_invoice => true)}
 	scope :credit_note, lambda {where(:doc_credit => true)}
@@ -34,6 +37,70 @@ class Invoice < ActiveRecord::Base
 		else
 			1
 		end
+	end
+
+	def clean_amount
+		clean_amount = 0
+		self.sorting_details.clean.each do |detail|
+			if detail.article.prices.last.washing.present?
+				clean_amount += (detail.total_cups * detail.article.prices.last.washing)
+			else
+				clean_amount
+			end
+		end
+		clean_amount
+	end
+
+	def very_dirty_amount
+		very_dirty_amount = 0
+		self.sorting_details.very_dirty.each do |detail|
+			if detail.article.prices.last.handwash.present?
+				very_dirty_amount += (detail.total_cups * detail.article.prices.last.handwash)
+			else
+				very_dirty_amount
+			end
+		end
+		very_dirty_amount
+	end
+
+	def broken_amount
+		broken_amount = 0
+		self.sorting_details.broken.each do |detail|
+			if detail.article.prices.last.deposit.present?
+				broken_amount += (detail.total_cups * detail.article.prices.last.deposit)
+			else
+				broken_amount
+			end
+		end
+		broken_amount
+	end
+
+	def handling_amount
+		handling_amount = 0
+		self.sorting_details.handling.each do |detail|
+			if detail.article.prices.last.handling.present?
+				handling_amount += (detail.total_cups * detail.article.prices.last.handling)
+			else
+				handling_amount
+			end
+		end
+		handling_amount
+	end
+
+	def article_amount
+		article_amount = 0
+		self.offer.offer_articles.each do |article|
+			if Price.regular.where(:article_id => article.article.id).any? && Price.regular.where(:article_id => article.article.id).last.sell.present?
+				article_amount += (article.quantity * Price.regular.where(:article_id => article.article.id).last.sell) if Price.regular.where(:article_id => article.article.id).last.sell.present?
+			else
+				article_amount
+			end
+		end
+		article_amount
+	end
+
+	def total_htva
+		clean_amount + broken_amount + very_dirty_amount + handling_amount + self.offer.transport_price + article_amount
 	end
 
 end
--- app/models/price.rb
+++ app/models/price.rb
@@ -1,4 +1,8 @@
 class Price < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :article
+
+	scope :regular, lambda {where(:regular => true)}
+	scope :negociated, lambda {where(:negociated => true)}
+
 end
