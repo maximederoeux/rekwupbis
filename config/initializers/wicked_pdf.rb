74aba3757a6755c9bd8075e83338e712f6ca7067
maximederoeux <mderoeux@yahoo.com>
2016-02-06 18:36:09 +0100

    PDF issue on heroku

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -9,6 +9,9 @@
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
 WickedPdf.config = {
+	if Rails.env == 'production' then
+    config.exe_path = Rails.root.to_s + "/bin/wkhtmltopdf"
+  else
  :wkhtmltopdf => '/home/ferache/.rbenv/versions/2.2.3/bin/wkhtmltopdf',
   # Path to the wkhtmltopdf executable: This usually isn't needed if using
   # one of the wkhtmltopdf-binary family of gems.
@@ -19,4 +22,19 @@ WickedPdf.config = {
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
   # layout: 'pdf.html',
+	end
 }
+
+# WickedPdf.config do |config|  
+#   if Rails.env == 'production' then
+#     config.exe_path = Rails.root.to_s + "/bin/wkhtmltopdf"
+#   else  ### Following allows for development on my MacBook or Linux box
+#     if /darwin/ =~ RUBY_PLATFORM then
+#       config.exe_path = '/usr/local/bin/wkhtmltopdf' 
+#     elsif /linux/ =~ RUBY_PLATFORM then
+#       config.exe_path = '/usr/bin/wkhtmltopdf' 
+#     else
+#       raise "UnableToLocateWkhtmltopdf"
+#     end
+#   end
+# end
