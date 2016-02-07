72fe5737ccddadb03f6853e8a6c05aa9493ad226
maximederoeux <mderoeux@yahoo.com>
2016-02-07 21:30:14 +0100

    idem

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -23,7 +23,7 @@ class UserMailer < ApplicationMailer
     @offer = offer
     @invoice = invoice
     @organizer = user
-    @url = 'http://clients.rekwup.be/invoices/#{@invoice.id}'
+    @url = 'http://clients.rekwup.be/invoices/<%= @invoice.id %>'
 
     mail(
       to: @organizer.email, 
