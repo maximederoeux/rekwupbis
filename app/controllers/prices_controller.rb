3ace650d01233557f2fee0be495238610ba3a03e
maximederoeux <mderoeux@yahoo.com>
2016-02-23 16:50:37 +0100

    Prices

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -44,7 +44,7 @@ class PricesController < ApplicationController
   def update
     respond_to do |format|
       if @price.update(price_params)
-        format.html { redirect_to :back, notice: 'Price was successfully updated.' }
+        format.html { redirect_to prices_path, notice: 'Price was successfully updated.' }
         format.json { render :show, status: :ok, location: @price }
       else
         format.html { render :edit }
