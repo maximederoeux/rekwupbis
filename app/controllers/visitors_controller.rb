88764ea0b2f6241401bca79541229a433da35d1b
maximederoeux <mderoeux@yahoo.com>
2016-06-28 12:18:43 +0200

    Idem

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,6 +4,7 @@ class VisitorsController < ApplicationController
   def index
     @users = User.all
     @staff = User.staff.order('name ASC')
+    @events = Event.all.order('event_start_date ASC')
     @user = current_user
     @articles = Article.all
     @rekwup_cups = @articles.where(:rekwup_cup => true)
